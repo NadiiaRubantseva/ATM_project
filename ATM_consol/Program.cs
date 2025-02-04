d0477865d1c7cd960166368e7bc355ed23e8281e
Nadiia Rubantseva <107259127+NadiiaRubantseva@users.noreply.github.com>
2025-02-05 01:27:16 +0200

    Closes #1

--- ATM_consol/Program.cs
+++ ATM_consol/Program.cs
@@ -6,12 +6,10 @@ namespace ATM_consol
     public class Program
     {
         const string id = "ia9234";
-
         public static void printText(string t)
         {
             Console.WriteLine(t);
         }
-
         static void Main(string[] args)
         {
             Money money = new Money();            
@@ -19,21 +17,24 @@ namespace ATM_consol
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
             int t = atm.FindATM(id);
-            if(t==-1)
+            if (t == -1)
             {
                 Console.WriteLine("Помилка!");
                 Console.WriteLine("На жаль цей банкомат більше не обслуговується нашим банком.");
                 return;
             }
             Console.WriteLine($"{atm.FindName(t)}, {atm.FindStreet(t)}\n");
-            money.RegisterHandler( new Money.AccountStateHandler(printText));
+            money.RegisterHandler(new Money.AccountStateHandler(printText));
+            
             Console.Write("Введіть номер карти: ");
             string card = Console.ReadLine();
             int atempt = 1;
-            while(money.LogInCard(card)==-1)
+            int i; // Declare variable to store login index
+            
+            while ((i = money.LogInCard(card)) == -1) // Store result in variable
             {
                 Console.WriteLine("Помилка!");
-                if (atempt==3)
+                if (atempt == 3)
                 {
                     Console.WriteLine("Кількість спроб закінчились.");
                     return;
@@ -42,10 +43,11 @@ namespace ATM_consol
                 card = Console.ReadLine();
                 atempt++;
             }
-            int i = money.LogInCard(card);
+
             Console.Write("Введіть пінкод: ");
             string pin = Console.ReadLine();
             atempt = 1;
+
             while (!money.LogInPin(i, pin))
             {
                 Console.WriteLine("Помилка!");
@@ -58,61 +60,53 @@ namespace ATM_consol
                 pin = Console.ReadLine();
                 atempt++;
             }
+
             bool temp = false;
-            do 
+            do
             {
                 Console.Write("Оберіть дію: 1 - Перегляд балансу; 2 - Зняття грошей; 3 - Зарахування коштів на карту; 4 - Перерахування коштів іншій людині;\n\t-> ");
                 int choose = int.Parse(Console.ReadLine());
                 switch (choose)
                 {
                     case 1:
-                        {
-                            money.ShowMoney(i);
-                            break;
-                        }
+                        money.ShowMoney(i);
+                        break;
                     case 2:
-                        {
-                            Console.WriteLine("\nВведіть суму: ");
-                            int cash = int.Parse(Console.ReadLine());
-                            money.TakeMoney(i, cash, t);
-                            break;
-                        }
+                        Console.WriteLine("\nВведіть суму: ");
+                        int cash = int.Parse(Console.ReadLine());
+                        money.TakeMoney(i, cash, t);
+                        break;
                     case 3:
-                        {
-                            Console.Write("\nВведіть суму: ");
-                            int cash = int.Parse(Console.ReadLine());
-                            money.GiveMoney(i, cash, t);
-                            break;
-                        }
+                        Console.Write("\nВведіть суму: ");
+                        cash = int.Parse(Console.ReadLine());
+                        money.GiveMoney(i, cash, t);
+                        break;
                     case 4:
+                        Console.Write("\nВведіть номер карти для перерахування: ");
+                        string cardSend = Console.ReadLine();
+                        int j; // Declare variable to store login index for recipient
+                        while ((j = money.LogInCard(cardSend)) == -1 || cardSend == money.getCard(i))
                         {
-                            Console.Write("\nВведіть номер карти для перерахування: ");
-                            string cardSend = Console.ReadLine();
-                            while (money.LogInCard(cardSend) == -1 || cardSend == money.getCard(i))
-                            {
-                                Console.WriteLine("Помилка!");
-                                Console.Write("Введіть номер карти для перерахування: ");
-                                cardSend = Console.ReadLine();
-                            }
-                            int j = money.LogInCard(cardSend);
-                            Console.Write($"Суму буде перерахованно для {money.getName(j)}.\n\t1 - Продовжити;  2(або інша будь-яка цифра) - Ні\n\t->");
-                            int ans = int.Parse(Console.ReadLine());
-                            if (ans != 1) break;
-                            Console.Write("\nВведіть суму: ");
-                            int cash = int.Parse(Console.ReadLine());
-                            money.SendMoney(i, cash, j);
-                            break;
+                            Console.WriteLine("Помилка!");
+                            Console.Write("Введіть номер карти для перерахування: ");
+                            cardSend = Console.ReadLine();
                         }
+                        Console.Write($"Суму буде перерахованно для {money.getName(j)}.\n\t1 - Продовжити;  2(або інша будь-яка цифра) - Ні\n\t->");
+                        int ans = int.Parse(Console.ReadLine());
+                        if (ans != 1) break;
+                        Console.Write("\nВведіть суму: ");
+                        cash = int.Parse(Console.ReadLine());
+                        money.SendMoney(i, cash, j);
+                        break;
                     default:
-                        {
-                            Console.WriteLine("Такої дії не існує.");
-                            break;
-                        }
+                        Console.WriteLine("Такої дії не існує.");
+                        break;
                 }
                 Console.Write("Чи бажаєте ви продовжити?\n\t1 - Так; 2(або інша будь-яка цифра) - Ні\n\t->");
                 int answer = int.Parse(Console.ReadLine());
-                temp = answer == 1 ? true : false;
-            } while(temp);
+                temp = answer == 1;
+            } while (temp);
+            
             Console.WriteLine("До побачення!");
         }
     }
